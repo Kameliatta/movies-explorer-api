8cb4feae6c4ef41e98367b232e927d2c323c52a1
Ksenia Prozorova <niakam41@yandex.ru>
2022-03-27 17:28:48 +1200

    add min length for name

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const {
 router.get('/me', getUsers);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
   }),
 }), updateProfile);
